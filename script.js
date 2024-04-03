4f5c957bd03bc653c733e0b754cf4815ebb07263
Rango <gorankn89@gmail.com>
2024-04-03 11:34:04 +0200

    user get crud

--- script.js
+++ script.js
@@ -3,15 +3,40 @@ const app = express();
 const port = 3000;
 
 let users = [
-  { id: 1, name: "User1", group: "Group1", category: "Category1" },
-  { id: 2, name: "User2", group: "Group2", category: "Category2" },
+  {
+    id: "1",
+    password: "123",
+    name: "User1",
+    group: "Group1",
+    category: "Category1",
+  },
+  {
+    id: "2",
+    password: "321",
+    name: "User2",
+    group: "Group2",
+    category: "Category2",
+  },
   // Add more users as needed
 ];
 
 app.get("/users", (req, res) => {
-  res.json(users);
+  const id = req.query.id;
+  const password = req.query.password;
+  // Now you can use the id and password for further processing
+  // For example, you might want to find the user with this id and password
+  const user = users.find((user) => {
+    console.log(user.id === id);
+    console.log(user.password === password);
+    return user.id === id && user.password === password;
+  });
+  console.log(user);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
 });
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });

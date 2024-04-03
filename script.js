f6e6ab9ad8352c5c0a462ca29d83ef201cb18556
Rango <gorankn89@gmail.com>
2024-04-03 14:28:36 +0200

    cors3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, "build")));
-const port = 3000;
+const port = 3001;
 
 let users = [
   {
